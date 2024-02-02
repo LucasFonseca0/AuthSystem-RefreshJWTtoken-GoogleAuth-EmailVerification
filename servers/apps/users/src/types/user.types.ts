02d00d8b7cf6b6648e5077b499257b95c369e2d9
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-02-02 18:54:29 +0000

    entities e types created

---
+++ servers/apps/users/src/types/user.types.ts
@@ -0,0 +1,12 @@
+import { Field, ObjectType } from "@nestjs/graphql";
+
+@ObjectType()
+export class ErrorType{
+    @Field()
+    message:string
+
+    @Field({nullable:true})
+    code?:string
+
+}
+
