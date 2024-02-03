33287eca1fba8e15574f1de0f6656e7f165301e7
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-02-03 16:24:31 +0000

    feat: resolver

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,13 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { LoginDto, RegisterDto } from './dto/user.dto';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'prisma/Prisma.service';
+// import { Response } from 'express';
 
 @Injectable()
 export class UsersService {
- constructor(
-   private readonly jwtService:JwtService,
-  //private readonly prisma:
-  private readonly configService:ConfigService,
-  ){}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  //register user
+  async register(registerDto: RegisterDto) {
+    const { name, email, password } = registerDto;
+    const user = {
+      name,
+      email,
+      password,
+    };
+    return user;
+  }
+
+  //Login Service
+  async Login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const user = {
+      email,
+      password,
+    };
+    return user;
+  }
+
+  //get all users service
+  async getUsers() {
+
+    return this.prisma.user.findMany({});
+  }
 }
- 
